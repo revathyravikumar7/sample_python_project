8c8f7dcf35196badc7a5c55207f74ab2d6b886e9
RevathyRavikumar <revathyravikumar7@gmail.com>
2025-05-21 21:44:08 +0530

    Intro, syntax, comments

--- day1.py
+++ day1.py
@@ -1,3 +1,19 @@
-print("Hello world")
+import sys
+
+print("Hello world",sys.version)
+
+#print(sys.version)
+a = 1
+b = 2
+
+a, b, c, d = 1, 2, 3, 4
+
+a, b = input("Enter two values")
+if a==b:
+    print("Values are same")
+else:
+    print("Values are diff")
+
+
 
 
